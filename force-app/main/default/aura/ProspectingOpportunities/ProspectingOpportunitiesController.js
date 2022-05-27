d24d8607e2ddf2d9155f3e895fc2aed13bd0c541
Nathan Wenslaff <nathan.wenslaff@ministrybrands.com>
2022-05-26 19:56:10 -0500

    handle error states in component

--- force-app/main/default/aura/ProspectingOpportunities/ProspectingOpportunitiesController.js
+++ force-app/main/default/aura/ProspectingOpportunities/ProspectingOpportunitiesController.js
@@ -1,5 +1,13 @@
 ({
   init: function (component, event, helper) {
+    component.set("v.loading", true);
+
     helper.getRecords(component);
+  },
+
+  createRecord: function (component, event, helper) {
+    component.set("v.loading", true);
+
+    helper.createRecord(component, event, helper);
   }
 });
